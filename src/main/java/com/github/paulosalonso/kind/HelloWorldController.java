b7265ed494ea0502c09b2b5ec10e70f30d2e9bdd
Paulo Alonso <paulo.alonso@luizalabs.com>
2022-01-05 14:47:10 -0300

    Initial commit

---
+++ src/main/java/com/github/paulosalonso/kind/HelloWorldController.java
@@ -0,0 +1,27 @@
+package com.github.paulosalonso.kind;
+
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloWorldController {
+
+  @GetMapping
+  public DTO helloWorld() {
+    return DTO.builder()
+        .message("Hello World V3!")
+        .status("UNHEALTHY")
+        .build();
+  }
+
+  @Builder
+  @Getter
+  public static class DTO {
+    private String message;
+    private String status;
+  }
+}
---
+++ src/main/java/com/github/paulosalonso/kind/KindApplication.java
@@ -0,0 +1,13 @@
+package com.github.paulosalonso.kind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KindApplication.class, args);
+	}
+
+}
